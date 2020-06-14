da04f0ecc6a4012a81a15b001149bc7005abee4b
Thanawat Petchsri <thanawat.petchsri@gmail.com>
2020-06-14 19:14:28 +0700

    Add redux test

--- src/sw.js
+++ src/sw.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 
 importScripts(
-  'https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js'
+  'https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js'
 );
 
 workbox.core.skipWaiting();
